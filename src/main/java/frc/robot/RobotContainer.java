434eb4c674df68553171fbe71d57e35c36ebe954
Jason <jmartzloff@gmail.com>
2024-02-07 16:14:12 -0500

    fixed identing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -40,41 +40,39 @@ import com.pathplanner.lib.path.PathPlannerPath;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+
+    // The robot's subsystems
+    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+    private final ShooterSubsystem shootersubsystem = new ShooterSubsystem();
     private final SendableChooser<Command> autoChooser;
 
-    
-  // The robot's subsystems
-  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  private final ShooterSubsystem shootersubsystem = new ShooterSubsystem();
+    // Define the controller being used
+    XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
 
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+    // The container for the robot. Contains subsystems, OI devices, and commands.
+    public RobotContainer() {
+        
+        // 
+        autoChooser = AutoBuilder.buildAutoChooser();
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-autoChooser = AutoBuilder.buildAutoChooser();
+        // Another option that allows you to specify the default auto by its name
+        // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
 
-    // Another option that allows you to specify the default auto by its name
-    // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+        SmartDashboard.putData("Auto Chooser", autoChooser);
 
-    SmartDashboard.putData("Auto Chooser", autoChooser);
-
-    // NamedCommands.registerCommand("autoBalance", shootersubsystem.shooterON());
-    // NamedCommands.registerCommand("exampleCommand", shootersubsystem.shooterOFF());
-    
-
+        // NamedCommands.registerCommand("autoBalance", shootersubsystem.shooterON());
+        // NamedCommands.registerCommand("exampleCommand", shootersubsystem.shooterOFF());
 
         // Register Named Commands
         NamedCommands.registerCommand("autoBalance", shootersubsystem.intakeON());
         NamedCommands.registerCommand("exampleCommand", shootersubsystem.intakeOFF());
-     
 
-    // Configure the button bindings
-    configureButtonBindings();
+        // Configure the button bindings
+        configureButtonBindings();
 
-    // Configure default commands
-    m_robotDrive.setDefaultCommand(
+        // Configure default commands
+        m_robotDrive.setDefaultCommand(
+        
         // The left stick controls translation of the robot.
         // Turning is controlled by the X axis of the right stick.
         new RunCommand(
@@ -84,84 +82,35 @@ autoChooser = AutoBuilder.buildAutoChooser();
                 -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
                 true, true),
             m_robotDrive));
-  }
+    }
 
-  
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by
-   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
-   * subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
-   * passing it to a
-   * {@link JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    //A button: puts the wheels to X
-    new JoystickButton(m_driverController, Button.kA.value)
-        .whileTrue(new RunCommand(
-            () -> m_robotDrive.setX(),
-            m_robotDrive));
+    // Setup the controller buttons
+ 
+    private void configureButtonBindings() {
+        //A button: puts the wheels to X
+        new JoystickButton(m_driverController, Button.kA.value)
+            .whileTrue(new RunCommand(
+                () -> m_robotDrive.setX(),
+                m_robotDrive));
 
-    // Left Bumper: Turn shooter on/off
-    new JoystickButton(m_driverController, Button.kLeftBumper.value)
-        .onTrue(new RunCommand(()-> shootersubsystem.shooterFlip()));
+        // Left Bumper: Turn shooter on/off
+        new JoystickButton(m_driverController, Button.kLeftBumper.value)
+            .onTrue(new RunCommand(()-> shootersubsystem.shooterFlip()));
 
-    // Right Bumper: Turn intake on/off
-    new JoystickButton(m_driverController, Button.kRightBumper.value)
-        .onTrue(new RunCommand(()-> shootersubsystem.intakeFlip()));
-
-  }
+        // Right Bumper: Turn intake on/off
+        new JoystickButton(m_driverController, Button.kRightBumper.value)
+            .onTrue(new RunCommand(()-> shootersubsystem.intakeFlip()));
+    }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
-//   public Command getAutonomousCommand() {
-//     // Create config for trajectory
-//     TrajectoryConfig config = new TrajectoryConfig(
-//         AutoConstants.kMaxSpeedMetersPerSecond,
-//         AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-//         // Add kinematics to ensure max speed is actually obeyed
-//         .setKinematics(DriveConstants.kDriveKinematics);
 
-//     // An example trajectory to follow. All units in meters.
-//     Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-//         // Start at the origin facing the +X direction
-//         new Pose2d(0, 0, new Rotation2d(0)),
-//         // Pass through these two interior waypoints, making an 's' curve path
-//         List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-//         // End 3 meters straight ahead of where we started, facing forward
-//         new Pose2d(3, 0, new Rotation2d(0)),
-//         config);
 
-//     var thetaController = new ProfiledPIDController(
-//         AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-//     thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-//     SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-//         exampleTrajectory,
-//         m_robotDrive::getPose, // Functional interface to feed supplier
-//         DriveConstants.kDriveKinematics,
-
-//         // Position controllers
-//         new PIDController(AutoConstants.kPXController, 0, 0),
-//         new PIDController(AutoConstants.kPYController, 0, 0),
-//         thetaController,
-//         m_robotDrive::setModuleStates,
-//         m_robotDrive);
-
-//     // Reset odometry to the starting pose of the trajectory.
-//     m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
-
-//     // Run path following command, then stop at the end.
-    // return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false, false));
-//   }
-
-   public Command getAutonomousCommand() {
-     PathPlannerPath path = PathPlannerPath.fromPathFile("Example Path");
-    return new PathPlannerAuto("Example Auto");
-  }
-
+    public Command getAutonomousCommand() {
+        PathPlannerPath path = PathPlannerPath.fromPathFile("Example Path");
+        return new PathPlannerAuto("Example Auto");
+    }
 }
