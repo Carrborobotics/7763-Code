572b4b1e93386ab1713faa77eb56d03a09a7d86c
Jason <jmartzloff@gmail.com>
2024-02-13 18:55:19 -0500

    added some limelight skeleton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,33 +5,24 @@
 package frc.robot; 
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import java.util.List;
+import frc.robot.subsystems.Limelight;
+
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.path.PathPlannerPath;
+
 
 /*
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -45,6 +36,7 @@ public class RobotContainer {
     private final DriveSubsystem m_robotDrive = new DriveSubsystem();
     private final ShooterSubsystem shootersubsystem = new ShooterSubsystem();
     private final SendableChooser<Command> autoChooser;
+    private final Limelight m_vision = new Limelight("limelight");
 
     // Define the controller being used
     XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
@@ -52,12 +44,7 @@ public class RobotContainer {
     // The container for the robot. Contains subsystems, OI devices, and commands.
     public RobotContainer() {
         
-        // 
         autoChooser = AutoBuilder.buildAutoChooser();
-
-        // Another option that allows you to specify the default auto by its name
-        // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
-
         SmartDashboard.putData("Auto Chooser", autoChooser);
 
         // Register Named Commands
@@ -66,25 +53,25 @@ public class RobotContainer {
         NamedCommands.registerCommand("shooterON", shootersubsystem.shooterON());
         NamedCommands.registerCommand("shooterOFF", shootersubsystem.shooterOFF());
 
-        // Configure the button bindings
         configureButtonBindings();
+        configureAuto();
+        m_vision.getName();
 
         // Configure default commands
         m_robotDrive.setDefaultCommand(
-        
-        // The left stick controls translation of the robot.
-        // Turning is controlled by the X axis of the right stick.
-        new RunCommand(
-            () -> m_robotDrive.drive(
-                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
-                true, true),
-            m_robotDrive));
+            new RunCommand(
+                () -> m_robotDrive.drive(
+                    -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                    -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                    -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
+                    true, true),
+                m_robotDrive));
     }
 
-    // Setup the controller buttons
- 
+    private void configureAuto() {
+
+    }
+    
     private void configureButtonBindings() {
         //A button: puts the wheels to X
         new JoystickButton(m_driverController, Button.kA.value)
@@ -102,6 +89,7 @@ public class RobotContainer {
     
         SmartDashboard.putData("Two Piece Auto Test", new PathPlannerAuto("2 Piece Auto"));
         SmartDashboard.putData("Shooter Test", new PathPlannerAuto("shooter test"));
+<<<<<<< HEAD
         SmartDashboard.putData("Square Dance", new PathPlannerAuto("Square Auto"));
 
         // test that basic paths show up in the dash too
@@ -109,6 +97,8 @@ public class RobotContainer {
         AutoBuilder.followPath(path).schedule();
 
         return;
+=======
+>>>>>>> 300e9e5 (added some limelight skeleton)
     }
 
 
